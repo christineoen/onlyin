ec3f5d4d8cbfac13b6b2dd005ccb8497e98dcfdb
Christine Oen <oen.christine@gmail.com>
2014-11-12 14:40:52 -0600

    added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
+    devise_scope :user do
+      authenticated :user do
+        root :to => 'home#index', as: :authenticated_root
+      end
+      unauthenticated :user do
+        root :to => 'devise/sessions#new', as: :unauthenticated_root
+      end
+    end
+ 
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
