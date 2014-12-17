8cb56bac922216fb89620e04b241de819ca36c28
Christine Oen <oen.christine@gmail.com>
2014-12-17 16:25:40 -0600

    removed photo migration

--- db/migrate/20141112210421_create_photos.rb
+++
@@ -1,9 +0,0 @@
-class CreatePhotos < ActiveRecord::Migration
-  def change
-    create_table :photos do |t|
-      t.belongs_to :post
-      
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -34,12 +34,6 @@ ActiveRecord::Schema.define(version: 20141216213300) do
     t.datetime "updated_at"
   end
 
-  create_table "photos", force: true do |t|
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: true do |t|
     t.integer  "user_id"
     t.integer  "location_id"
