52d5529a4b4fd4531c6abe0bee99b84f50aabede
Christine Oen <oen.christine@gmail.com>
2014-12-17 20:10:18 -0600

    added associations to models, added posts controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
+  before_action :set_user, only: [:index]
 
   def index
+    @post = Post.new
+    @posts = Post.all
   end
   
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = current_user
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :location
+  has_many :votes
+  has_many :comments
   mount_uploader :file, FileUploader
 end
